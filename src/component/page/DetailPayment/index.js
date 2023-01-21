9e8c0105de366d3daf50aa5c1d5ef032749c77ec
Your Name <you@example.com>
2023-01-22 03:00:15 +0400

    adding some card in pembayaran page

--- src/component/page/DetailPayment/index.js
+++ src/component/page/DetailPayment/index.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../HomePage/navbar';
+import BackSign from '../fi_arrow-left.png';
+import Rectangle36 from '../Rectangle_36.jpg'
+import './style.css';
 
 export const DetailPayment = () => {
   return (
-    <div>
-        <h1>DetailPayment</h1>
-    </div>
+    <>
+      <div className='main-wrapper'>
+          <Navbar />
+
+          <div className='container'> 
+            <div className='wrapper-detail-payment'>
+                <div className='wrapper-pembayaran-right'>
+                    <img src={BackSign} />
+                    <a href='/cari-mobil' className='button-back'>Pembayaran</a>
+                </div>
+                <div className='wrapper-pembayaran-left'>
+                  <div className='method-payment'>
+                    <p className='payment-step-1'>1</p>
+                    <p>Pilih Metode</p>
+                    <img src={Rectangle36} />
+                  </div>
+                  <div className='method-payment'>
+                    <p className='payment-step'>2</p>
+                    <p>Bayar</p>
+                    <img src={Rectangle36} />
+                  </div>
+                  <div className='method-payment'>
+                    <p className='payment-step'>3</p>
+                    <p>Tiket</p>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+      </div>
+
+      <div className='container'>
+        <div className='detail-order'>
+          <div className='card'>
+              <div className='card-detail'>
+                <p className='judul-detail'>Detail Pesananmu</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
